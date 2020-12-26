babc6155b1a782d6537b979f542c9b8e36f64e08
Leonhard S <leonhard-sei@outlook.com>
2020-12-26 08:14:14 +0100

    Create API server
    This example runs, but does not yet use any API data.

--- apl_backend/__main__.py
+++ apl_backend/__main__.py
@@ -15,6 +15,7 @@ import logging
 
 import auraxium
 
+from ._api import ApiHost
 from ._db import DatabaseHandler
 from ._map import MapHandler
 from ._server import BackendServer
@@ -29,8 +30,10 @@ DEFAULT_DB_USER = 'postgres'
 # Logging configuration
 fmt = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 fh_ = logging.FileHandler(filename='debug.log', encoding='utf-8', mode='w+')
+fh_api = logging.FileHandler(filename='api.log', encoding='utf-8', mode='w+')
 sh_ = logging.StreamHandler()
 fh_.setFormatter(fmt)
+fh_api.setFormatter(fmt)
 sh_.setFormatter(fmt)
 
 
@@ -97,6 +100,11 @@ if __name__ == '__main__':
         arx_log.setLevel(max(log_level, logging.INFO))
         arx_log.addHandler(fh_)
         arx_log.addHandler(sh_)
+        # Add another logger for the API component
+        api_log = logging.getLogger('api')
+        api_log.setLevel((max(log_level, logging.INFO)))
+        api_log.addHandler(fh_api)
+        api_log.addHandler(sh_)
     # Run utility
     loop = asyncio.get_event_loop()
     loop.create_task(main(**kwargs))
